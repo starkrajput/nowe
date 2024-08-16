ad5992acb704eaa4641e196cc1d009e919cafc94
Aditya Singh <82673462+starkrajput@users.noreply.github.com>
2024-08-16 20:35:02 +0530

    Update Testimonial.jsx

--- ourweb/src/Component/Testimonial.jsx
+++ ourweb/src/Component/Testimonial.jsx
@@ -1,81 +1,79 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import './Testimonial.css';
-import Testi1 from '../assets/Vedio/Testi1.jpg';
-import Testi2 from '../assets/Vedio/Testi2.jpg';
-import Testi3 from '../assets/Vedio/Testi3.jpg';
 
 const Testimonial = () => {
     return (
         <div>
             {/* Testimonial Header */}
-            <div className="containerS text-center mt-5 testimonial-header">
+            <div className="containerS text-center mt-5 testimonial-header" style={{ marginTop: '30px', background: 'black', borderRadius:'8px',height:'90px' ,color:'white'}}>
                 <h2>Testimonials</h2>
-                <p className="container testimonial-description" style={{ height: 'auto' }}>
+                <p className="containerS testimonial-description" style={{ height: 'auto' }}>
                     When we say we are all about our people, we really mean it. The idea of "taking care of people" both our employees
                     and our clients has been built into the way we do business.
                 </p>
             </div>
 
-            {/* Testimonial Cards */}
-            <div className="containerS">
+          
+            <div className="container" style={{height :'auto'} }>
                 <div className="row">
                     <div className="col-lg-4">
                         <div className="card">
                             <div className="face front-face">
-                                <img src={ Testi1}
-                                    alt="" className="profile" />
-                                <div className="pt-3 text-uppercase name">
-                                    Dr. Klaus Schmid
-                                </div>
-                                <div className="designation">Marketing Director, Technova GmbH, Germany</div>
+                                <img
+                                    src="https://images.unsplash.com/photo-1557862921-37829c790f19?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1051&q=80"
+                                    alt=""
+                                    className="profile"
+                                />
+                                <div className="pt-3 text-uppercase name">Robert Garrison</div>
+                                <div className="designation">Android Developer</div>
                             </div>
                             <div className="face back-face">
                                 <span className="fas fa-quote-left"></span>
                                 <div className="testimonial">
-                                    RSV Technologies has been an invaluable partner in our digital transformation journey. Their expertise in crafting a robust digital marketing strategy propelled our brand to new heights. The team's dedication to understanding our business objectives and delivering tailored solutions is commendable.
-
-
+                                    I made back the purchase price in just 48 hours! Thank you for making it painless, pleasant. The service was excellent. I will refer everyone I know.
                                 </div>
                                 <span className="fas fa-quote-right"></span>
                             </div>
                         </div>
                     </div>
+
                     <div className="col-lg-4">
                         <div className="card">
                             <div className="face front-face">
-                                <img src={Testi2}
-                                    alt="" className="profile" />
-                                <div className="pt-3 text-uppercase name">
-                                    Pierre Dubois
-                                </div>
-                                <div className="designation">CEO, La Mode Parisienne, France</div>
+                                <img
+                                    src="https://images.unsplash.com/photo-1600486913747-55e5470d6f40?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
+                                    alt=""
+                                    className="profile"
+                                />
+                                <div className="pt-3 text-uppercase name">Jeffery Kennan</div>
+                                <div className="designation">Full Stack Developer</div>
                             </div>
                             <div className="face back-face">
                                 <span className="fas fa-quote-left"></span>
                                 <div className="testimonial">
-                                    We were impressed by RSV Technologies' ability to develop a mobile app that perfectly aligned with our brand identity. Their app development team was highly skilled and responsive to our needs. The app has not only enhanced our customer experience but also driven substantial revenue growth. 
+                                    Really good, you have saved our business! I made back the purchase price in just 48 hours! Man, this thing is getting better and better as I learn more about it.
                                 </div>
                                 <span className="fas fa-quote-right"></span>
                             </div>
                         </div>
                     </div>
+
                     <div className="col-lg-4">
                         <div className="card">
                             <div className="face front-face">
-                                <img src={Testi3}
-                                    alt="" className="profile" />
-                                <div className="pt-3 text-uppercase name">
-                                    Mr. Rajeev Patel
-                                </div>
-                                <div className="designation">Marketing Head, Desai Enterprises, Mumbai, India</div>
+                                <img
+                                    src="https://images.unsplash.com/photo-1614574762522-6ac2fbba2208?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MjY2fHxtYW58ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+                                    alt=""
+                                    className="profile"
+                                />
+                                <div className="pt-3 text-uppercase name">Issac Maxwell</div>
+                                <div className="designation">Finance Director</div>
                             </div>
                             <div className="face back-face">
                                 <span className="fas fa-quote-left"></span>
                                 <div className="testimonial">
-                                    RSV Technologies has been instrumental in helping us establish a strong online presence. Their digital marketing strategies have yielded impressive results, and their team's dedication to understanding our business is unparalleled. We are particularly satisfied with their ability to create engaging content that resonates with our target audience.
-
-
+                                    Account keeper is the most valuable business research we have EVER purchased. Without electrician, we would have gone bankrupt by now.
                                 </div>
                                 <span className="fas fa-quote-right"></span>
                             </div>
